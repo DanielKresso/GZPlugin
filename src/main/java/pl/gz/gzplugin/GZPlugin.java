fc1d32ed0774559f67433220b97eb538e089e9b8
Dawcioo0055 <52385493+Dawcioo0055@users.noreply.github.com>
2020-11-09 15:57:37 +0100

    Add files via upload

---
+++ src/main/java/pl/gz/gzplugin/GZPlugin.java
@@ -0,0 +1,17 @@
+package pl.gz.gzplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GZPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
